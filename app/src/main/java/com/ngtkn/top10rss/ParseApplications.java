a46a18e317c575aa6e4284ae9ebd74fd08debe09
ngt kn <nagata.ken78@gmail.com>
2018-06-06 20:59:01 -0700

    Update parse method in ParseApplications
    updates incomplete code
    feat

--- app/src/main/java/com/ngtkn/top10rss/ParseApplications.java
+++ app/src/main/java/com/ngtkn/top10rss/ParseApplications.java
@@ -1,5 +1,7 @@
 package com.ngtkn.top10rss;
 
+import android.util.Log;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
@@ -20,7 +22,7 @@ public class ParseApplications {
 
     public boolean parse(String xmlData) {
         boolean status = true;
-        FeedEntry currentRecord;
+        FeedEntry currentRecord = null;
         boolean inEntry = false;
         String textValue = "";
 
@@ -31,7 +33,43 @@ public class ParseApplications {
             xpp.setInput(new StringReader(xmlData));
             int eventType = xpp.getEventType();
             while(eventType != XmlPullParser.END_DOCUMENT){
-                
+                String tagName = xpp.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        Log.d(TAG, "parse: Starting tag for " + tagName);
+                        if("entry".equalsIgnoreCase(tagName)){
+                            inEntry = true;
+                            currentRecord = new FeedEntry();
+                        }
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        textValue = xpp.getText();
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        Log.d(TAG, "parse: Ending tag for " + tagName);
+                        if(inEntry) {
+                            if ("entry".equalsIgnoreCase(tagName)){
+                                applications.add(currentRecord);
+                                inEntry = false;
+                            } else if ("name".equalsIgnoreCase(tagName)){
+                                currentRecord.setName(textValue);
+                            } else if ("artist".equalsIgnoreCase(tagName)){
+                                currentRecord.setArtist(textValue);
+                            } else if ("releaseDate".equalsIgnoreCase(tagName)){
+                                currentRecord.setReleaseDate(textValue);
+                            } else if ("summary".equalsIgnoreCase(tagName)){
+                                currentRecord.setSummary(textValue);
+                            } else if ("image".equalsIgnoreCase(tagName)){
+                                currentRecord.setImageURL(textValue);
+                            }
+                        }
+                        break;
+
+                    default:
+                }
+                eventType = xpp.next();
             }
         } catch (Exception e) {
             status = false;
