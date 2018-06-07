c40b26aef728df758faf0afa3c140f4e5e8cc559
ngt kn <nagata.ken78@gmail.com>
2018-06-07 00:10:37 -0700

    adds custom view adapter, and class for view holder pattern
    feat

--- app/src/main/java/com/ngtkn/top10rss/MainActivity.java
+++ app/src/main/java/com/ngtkn/top10rss/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,16 +17,18 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private ListView listApps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listApps = findViewById(R.id.xmlListView);
 
         Log.d(TAG, "onCreate: starting Asynctask");
         DownloadData downloadData = new DownloadData();
         downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
-        Log.d(TAG, "oncreate: Done");
+        Log.d(TAG, "onCreate: Done");
 
     }
 
@@ -36,6 +40,15 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "onPostExecute: paramater is " + s);
             ParseApplications parseApplications = new ParseApplications();
             parseApplications.parse(s);
+
+//            ArrayAdapter<FeedEntry> arrayAdapter = new ArrayAdapter<FeedEntry>(
+//                    MainActivity.this, R.layout.list_item, parseApplications.getApplications());
+//            listApps.setAdapter(arrayAdapter);
+
+            FeedAdapter feedAdapter = new FeedAdapter(MainActivity.this,
+                    R.layout.list_record, parseApplications.getApplications());
+
+            listApps.setAdapter(feedAdapter);
         }
 
         @Override
