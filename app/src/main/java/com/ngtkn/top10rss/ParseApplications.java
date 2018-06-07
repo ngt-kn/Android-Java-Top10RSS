477d770c0e48112e556e3ff6c7f882fd8a7f2d45
ngt kn <nagata.ken78@gmail.com>
2018-06-06 20:20:46 -0700

    add FeedEntry and ParseApplications classes

--- app/src/main/java/com/ngtkn/top10rss/ParseApplications.java
+++ app/src/main/java/com/ngtkn/top10rss/ParseApplications.java
@@ -1,4 +1,16 @@
 package com.ngtkn.top10rss;
 
+import java.util.ArrayList;
+
 public class ParseApplications {
+    private static final String TAG = "ParseApplications";
+    private ArrayList<FeedEntry> applications;
+
+    public ParseApplications() {
+        this.applications = new ArrayList<>();
+    }
+
+    public ArrayList<FeedEntry> getApplications() {
+        return applications;
+    }
 }
